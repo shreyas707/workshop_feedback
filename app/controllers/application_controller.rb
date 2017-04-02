c9065f2d69be33ca397563ee4573b4c53cc5be23
Shreyas S <hello@shreyas.im>
2017-04-03 03:08:07 +0530

    redirect record not found to 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    redirect_to ('/404')
+  end
+
   protected
   def authenticate_user!
     	if user_signed_in?
@@ -11,9 +17,4 @@ class ApplicationController < ActionController::Base
       		redirect_to new_user_session_path, :notice => 'You need to login to access the page.'
     	end
   end
-
-   # rescue_from Exception do
-   #   redirect_to root_path, alert: "Sorry something went wrong"
-   # end
-
 end
