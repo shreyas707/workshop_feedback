380ef326b4b83a9b624f872cecf3ebe9e0bd09fb
Shreyas S <hello@shreyas.im>
2017-04-02 12:29:16 +0530

    app controller exception for 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-protected
-	def authenticate_user!
+  protected
+  def authenticate_user!
     	if user_signed_in?
       		super
     	else
       		redirect_to new_user_session_path, :notice => 'You need to login to access the page.'
     	end
-    end
+  end
+
+   rescue_from Exception do
+     redirect_to root_path, alert: "Sorry something went wrong"
+   end
+
 end
